e89c8cef643f33e1033a318c173ff4d5bbd5f551
Gerson Garcia <gersonrangelgarciapc@gmail.com>
2022-11-02 21:29:27 -0300

    feat: implementando novo metodo

--- appRepubliquei.Domain/Contracts/Repository/IUsuarioRepository.cs
+++ appRepubliquei.Domain/Contracts/Repository/IUsuarioRepository.cs
@@ -19,5 +19,7 @@ namespace appRepubliquei.Domain.Contracts.Repository
         Task InserirUsuario(string nome, string sobrenome, string senha, string cpf, string estadoCivil, DateTime dataNascimento);
 
         Task<EnderecoUsuario> ObterUltimoRegistroInserido();
+
+        Task CrieiEssaAquiPorCima();
     }
 }
