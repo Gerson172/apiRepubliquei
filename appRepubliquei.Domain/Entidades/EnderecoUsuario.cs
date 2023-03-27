905bd670564dc91b2c4e0f6e968dc84886dbdba2
Gerson Garcia <gersonrangelgarciapc@gmail.com>
2023-03-27 10:19:41 -0300

    teste

--- appRepubliquei.Domain/Entidades/EnderecoUsuario.cs
+++ appRepubliquei.Domain/Entidades/EnderecoUsuario.cs
@@ -13,7 +13,7 @@ namespace appRepubliquei.Domain.Entidades
         public int ID { get; set; }
         public int CEP { get; set; }
         public string Estado { get; set; }
-        public string Cidade { get; set; }
+        public string Cidade { get; set; }  
         public string Bairro { get; set; }
         public string Logradouro { get; set; }
         public string Numero { get; set; }
